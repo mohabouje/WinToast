5edd467283792a632fb0291621ae43aa549ed685
Mohammed Boujemaoui <mohabouje@gmail.com>
2016-10-01 13:47:43 +0200

    Cleaning code

--- src/WinToastLib/WinToastTemplate.cpp
+++ src/WinToastLib/WinToastTemplate.cpp
@@ -4,29 +4,17 @@
 
 WinToastTemplate::WinToastTemplate(int txtFieldCount, bool hasImage) :
 	_textFieldsCount(txtFieldCount),
-	_textFields(txtFieldCount, L""),
-	_hasImage(hasImage),
-	_imagePath(L"")
+	_hasImage(hasImage)
 {
 	initComponentsFromConfiguration();
 }
 
-WinToastTemplate::WinToastTemplate(const WinToastTemplateType& type) : _type(type)
+WinToastTemplate::WinToastTemplate(const WinToastTemplateType& type) : 
+	_type(type)
 {
 	initComponentsFromType();
 }
 
-
-void WinToastTemplate::initComponentsFromType() {
-	_hasImage = _type < ToastTemplateType_ToastText01;
-	_textFieldsCount = (_hasImage ? _type  : _type - ToastTemplateType_ToastText01) + 1;
-	_textFields.reserve(_textFieldsCount);
-}
-
-void WinToastTemplate::initComponentsFromConfiguration() {
-	_textFields.reserve(_textFieldsCount);
-	_type = static_cast<WinToastTemplateType> ((_textFieldsCount - 1) + (_hasImage ?  0 :  ToastTemplateType_ToastText01));
-}
 
 WinToastTemplate::~WinToastTemplate()
 {
@@ -39,6 +27,17 @@ void WinToastTemplate::setTextField(const wstring& txt, int pos) {
 }
 void WinToastTemplate::setImagePath(const wstring& imgPath) {
 	if (!_hasImage)
-		_hasImage != _hasImage;
+		return;
 	_imagePath = imgPath;
+}
+
+void WinToastTemplate::initComponentsFromType() {
+	_hasImage = _type < ToastTemplateType_ToastText01;
+	_textFieldsCount = (_hasImage ? _type : _type - ToastTemplateType_ToastText01) + 1;
+	_textFields.reserve(_textFieldsCount);
+}
+
+void WinToastTemplate::initComponentsFromConfiguration() {
+	_textFields.reserve(_textFieldsCount);
+	_type = static_cast<WinToastTemplateType> ((_textFieldsCount - 1) + (_hasImage ? 0 : ToastTemplateType_ToastText01));
 }
