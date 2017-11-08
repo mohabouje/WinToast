d08280c9bf23f690cc1a90d9201724e80216a18a
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-11-08 17:10:53 +0100

    Console Example: add exit codes to indicate what happened
    Eventually, this example wants to act as a helper for scripts. The
    easiest way for scripts to handle user feedback is by inspecting the
    exit code.
    
    So for now, this is the table of exit codes and what they mean:
    
    0	user clicked on the toast
    1	user dismissed the toast
    2	toast timed out
    3	application hid the toast
    4	toast was not activated
    5	toast failed
    6	system does not support toasts
    7	unhandled option
    8	multiple texts were provided
    9	toast notification manager initialization failure
    10	toast could not be launched
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- example/console-example/main.cpp
+++ example/console-example/main.cpp
@@ -6,27 +6,33 @@ class CustomHandler : public IWinToastHandler {
 public:
     void toastActivated() const {
         std::wcout << L"The user clicked in this toast" << std::endl;
+        exit(0);
     }
 
     void toastDismissed(WinToastDismissalReason state) const {
         switch (state) {
         case UserCanceled:
             std::wcout << L"The user dismissed this toast" << std::endl;
+            exit(1);
             break;
         case TimedOut:
             std::wcout << L"The toast has timed out" << std::endl;
+            exit(2);
             break;
         case ApplicationHidden:
             std::wcout << L"The application hid the toast using ToastNotifier.hide()" << std::endl;
+            exit(3);
             break;
         default:
             std::wcout << L"Toast not activated" << std::endl;
+            exit(4);
             break;
         }
     }
 
     void toastFailed() const {
         std::wcout << L"Error showing current toast" << std::endl;
+        exit(5);
     }
 };
 
@@ -34,7 +40,7 @@ int wmain(int argc, LPWSTR *argv)
 {
     if (WinToast::isCompatible()) {
         std::wcerr << L"Error, your system in not supported!" << std::endl;
-        return 1;
+        return 6;
     }
 
     LPWSTR text = L"Hello, world!", imagePath = NULL;
@@ -45,20 +51,20 @@ int wmain(int argc, LPWSTR *argv)
             imagePath = argv[++i];
         else if (argv[i][0] == L'-') {
             std::wcout << L"Unhandled option: " << argv[i] << std::endl;
-            return 1;
+            return 7;
         }
         else if (i + 1 == argc)
             text = argv[i];
         else {
             std::wcerr << L"Cannot handle multiple texts for now" << std::endl;
-            return 1;
+            return 8;
         }
 
     WinToast::instance()->setAppName(L"Console WinToast Example");
     WinToast::instance()->setAppUserModelId(L"WinToast Console Example");
     if (!WinToast::instance()->initialize()) {
         std::wcerr << L"Error, your system in not compatible!" << std::endl;
-        return 1;
+        return 9;
     }
 
     WinToastTemplate templ;
@@ -73,11 +79,11 @@ int wmain(int argc, LPWSTR *argv)
 
     if (WinToast::instance()->showToast(templ, new CustomHandler()) < 0) {
         std::wcerr << L"Could not launch your toast notification!";
-        return 1;
+        return 10;
     }
 
     // Give the handler a chance for 15 seconds
     Sleep(15000);
 
-    return 0;
+    exit(2);
 }
