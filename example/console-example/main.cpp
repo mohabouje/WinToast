15b5206f419bfefc938b0911bcd00dc506e16129
Mohammed Boujemaoui Boulaghmoudi <mohabouje@gmail.com>
2018-02-09 13:34:43 +0100

    Adding an option to set up the attribution text & sound from the command app.

--- example/console-example/main.cpp
+++ example/console-example/main.cpp
@@ -1,4 +1,5 @@
 #include "wintoastlib.h"
+#include <string>     // std::string, std::stoi
 
 using namespace WinToastLib;
 
@@ -66,6 +67,8 @@ enum Results {
 #define COMMAND_HELP		L"--help"
 #define COMMAND_IMAGE		L"--image"
 #define COMMAND_SHORTCUT	L"--only-create-shortcut"
+#define COMMAND_AUDIOSTATE  L"--audio-state"
+#define COMMAND_ATTRIBUTE   L"--attribute"
 
 void print_help() {
 	std::wcout << "WinToast Console Example [OPTIONS]" << std::endl;
@@ -76,8 +79,10 @@ void print_help() {
 	std::wcout << "\t" << COMMAND_EXPIREMS << L" : Set the default expiration time" << std::endl;
 	std::wcout << "\t" << COMMAND_TEXT << L" : Set the text for the notifications" << std::endl;
 	std::wcout << "\t" << COMMAND_IMAGE << L" : set the image path" << std::endl;
+    std::wcout << "\t" << COMMAND_ATTRIBUTE << L" : set the attribute for the notification" << std::endl;
 	std::wcout << "\t" << COMMAND_SHORTCUT << L" : create the shortcut for the app" << std::endl;
-	std::wcout << "\t" << COMMAND_HELP << L" : Print the help description" << std::endl;
+    std::wcout << "\t" << COMMAND_AUDIOSTATE << L" : set the audio state: Default = 0, Silent = 1, Loop = 2" << std::endl;
+    std::wcout << "\t" << COMMAND_HELP << L" : Print the help description" << std::endl;
 }
 
 
@@ -93,11 +98,17 @@ int wmain(int argc, LPWSTR *argv)
         return Results::SystemNotSupported;
     }
 
-    LPWSTR appName = L"Console WinToast Example", appUserModelID = L"WinToast Console Example", text = NULL, imagePath = NULL;
+    LPWSTR appName = L"Console WinToast Example",
+        appUserModelID = L"WinToast Console Example",
+        text = NULL,
+        imagePath = NULL,
+        attribute = L"default";
     std::vector<std::wstring> actions;
     INT64 expiration = 0;
+   
 
     bool onlyCreateShortcut = false;
+    WinToastTemplate::AudioOption audioOption = WinToastTemplate::Default;
 
     int i;
 	for (i = 1; i < argc; i++)
@@ -113,8 +124,12 @@ int wmain(int argc, LPWSTR *argv)
             appUserModelID = argv[++i];
 		else if (!wcscmp(COMMAND_TEXT, argv[i]))
 			text = argv[++i];
+        else if (!wcscmp(COMMAND_ATTRIBUTE, argv[i]))
+            attribute = argv[++i];
 		else if (!wcscmp(COMMAND_SHORTCUT, argv[i]))
 			onlyCreateShortcut = true;
+        else if (!wcscmp(COMMAND_AUDIOSTATE, argv[i]))
+            audioOption = static_cast<WinToastTemplate::AudioOption>(std::stoi(argv[++i]));
 		else if (!wcscmp(COMMAND_HELP, argv[i])) {
 			print_help();
 			return 0;
@@ -146,7 +161,9 @@ int wmain(int argc, LPWSTR *argv)
     bool withImage = (imagePath != NULL);
 	WinToastTemplate templ( withImage ? WinToastTemplate::ImageAndText02 : WinToastTemplate::Text02);
 	templ.setTextField(text, WinToastTemplate::FirstLine);
-    
+    templ.setAudioOption(audioOption);
+    templ.setAttributionText(attribute);
+
 	for (auto const &action : actions)
         templ.addAction(action);
     if (expiration)
@@ -158,9 +175,7 @@ int wmain(int argc, LPWSTR *argv)
     // Loop and Silent are mutually exclusive. Also note that a custom sound, set via
     // WinToastTemplate.setAudioPath(), must be ms-appx:// or ms-appdata:// path.
     //
-    templ.setAudioOption(WinToastTemplate::AudioOption::Silent);
 
-    templ.setAttributionText(L"via SMS");
 
     if (WinToast::instance()->showToast(templ, new CustomHandler()) < 0) {
         std::wcerr << L"Could not launch your toast notification!";
