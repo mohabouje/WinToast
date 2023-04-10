42dbb68f9d380333e8810d21e52157105891c6bc
Mohammed Boujemaoui <mohabouje@gmail.com>
2023-04-10 11:22:49 +0200

    Updating license.txt to add information about the version number

--- src/wintoastlib.cpp
+++ src/wintoastlib.cpp
@@ -1,4 +1,7 @@
-/* * Copyright (C) 2016-2023 Mohammed Boujemaoui <mohabouje@gmail.com>
+/** 
+ * MIT License
+ *
+ * Copyright (C) 2016-2023 WinToast v1.3.0 - Mohammed Boujemaoui <mohabouje@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
